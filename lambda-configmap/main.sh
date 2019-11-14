a43f4970452d492d1a3d31730688bb93fe7f657d
sthapa <thapa.suraj751@gmail.com>
2019-11-14 16:05:55 +1100

    Check request type

--- lambda-configmap/main.sh
+++ lambda-configmap/main.sh
@@ -2,25 +2,32 @@
 
 source libs.sh
 
-# Retrieve the eks cluster name
-cluster_name=$(echo $1 | jq -r '.ResourceProperties.cluster_name | select(type == "string")')
-if [ -n "${cluster_name}" ];then
-    echo "Updating kubeconfig - cluster_name=$cluster_name"
-    update_kubeconfig "$cluster_name" || exit 1
-    echo "cluster_name=${cluster_name}" > /tmp/.env.config
-else
-    echo "Unable to find 'cluster_name' key!"
-fi
+# Retrieve the request type
+request_type=$(echo $1 | jq -r '.ResourceProperties.RequestType | select(type == "string")')
 
-# Retrieve the iam role arn
-iam_role_arn=$(echo $1 | jq -r '.ResourceProperties.iam_role_arn | select(type == "string")')
-if [ -n "${iam_role_arn}" ]; then
-    configMap=$(get_config_map $iam_role_arn)
+if test "$request_type" && [ "$request_type" != "Delete" ]; then
+    echo "RequestType: $request_type"
 
-    echo "$configMap"
-    get_contex
-    echo "$configMap" | kubectl apply -f - 2>&1
-    get_all
-else
-    echo "Unable to find 'iam_role_arn' key!"
+    # Retrieve the eks cluster name
+    cluster_name=$(echo $1 | jq -r '.ResourceProperties.cluster_name | select(type == "string")')
+    if [ -n "${cluster_name}" ];then
+        echo "Updating kubeconfig - cluster_name=$cluster_name"
+        update_kubeconfig "$cluster_name" || exit 1
+        echo "cluster_name=${cluster_name}" > /tmp/.env.config
+    else
+        echo "Unable to find 'cluster_name' key!"
+    fi
+
+    # Retrieve the iam role arn
+    iam_role_arn=$(echo $1 | jq -r '.ResourceProperties.iam_role_arn | select(type == "string")')
+    if [ -n "${iam_role_arn}" ]; then
+        configMap=$(get_config_map $iam_role_arn)
+
+        echo "$configMap"
+        get_contex
+        echo "$configMap" | kubectl apply -f - 2>&1
+        get_all
+    else
+        echo "Unable to find 'iam_role_arn' key!"
+    fi
 fi
